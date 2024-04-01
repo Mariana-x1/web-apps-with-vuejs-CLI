ad5962e968bbd078eb5c07f3c5802f8843b74874
Mari Astamadze <marihamburg321@gmail.com>
2024-04-01 15:43:19 +0200

    add LayoutHeader component local

--- 04-header-and-booklist-component/src/App.vue
+++ 04-header-and-booklist-component/src/App.vue
@@ -1,14 +1,47 @@
 <template>
+  <LayoutHeader />
   <b><HelloWorld msg="hello" /></b>
 </template>
 
 <script>
+import LayoutHeader from "./components/LayoutHeader.vue";
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    LayoutHeader,
   },
 };
 </script>
+<style>
+:root {
+  --primary: #6a1cc3;
+  --primary-dark: #370b68;
+  --secondary: #f39c12;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+h1,
+h2,
+h3,
+h4 {
+  color: var(--primary);
+  margin-bottom: 15px;
+}
+
+.inside {
+  margin: 10px;
+}
+
+@media only screen and (min-width: 1140px) {
+  .inside {
+    width: 960px;
+    margin: 0 auto;
+  }
+}
+</style>
