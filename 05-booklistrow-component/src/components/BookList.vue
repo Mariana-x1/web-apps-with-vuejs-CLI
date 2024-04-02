902038323b8ffa0afffad7a5e878e8eb586db6e8
Mari Astamadze <marihamburg321@gmail.com>
2024-04-02 10:24:44 +0200

    add BookListRow component and styling

--- 05-booklistrow-component/src/components/BookList.vue
+++ 05-booklistrow-component/src/components/BookList.vue
@@ -9,13 +9,26 @@
           <th class="table-item__table-head--action">&nbsp;</th>
         </tr>
       </thead>
+      <tbody>
+        <BookListRow
+          v-for="book in books"
+          :key="book.isbn"
+          :title="book.title"
+          :isbn="book.isbn"
+          class="table-item__table-row"
+        />
+      </tbody>
     </table>
   </section>
 </template>
 
 <script>
+import BookListRow from "./BookListRow.vue";
+
 export default {
-  components: {},
+  components: {
+    BookListRow,
+  },
   data() {
     return {
       books: [
@@ -56,3 +69,55 @@ export default {
   },
 };
 </script>
+<style scoped>
+.table-item__table {
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+  width: 100%;
+}
+.table-item__table-head-name {
+  width: 65%;
+}
+.table-item__table-head-isbn {
+  width: 20%;
+}
+.table-item__table-head-actions {
+  width: 15%;
+}
+.table-item__table-row button {
+  opacity: 0;
+  padding: 5px;
+  transition: opacity 500ms;
+  cursor: pointer;
+  border-radius: 5px;
+}
+
+.table-item__table thead tr {
+  background-color: var(--primary);
+  color: #ffffff;
+  text-align: left;
+}
+.table-item__table th {
+  padding: 12px 15px;
+}
+.table-item__table tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+.table-item__table tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+.table-item__table tbody tr:last-of-type {
+  border-bottom: 2px solid var(--primary);
+}
+.table-item__table tbody tr.active-row {
+  font-weight: bold;
+  color: var(--primary);
+}
+.table-item__hl {
+  margin-top: 1rem;
+  padding-bottom: 0.4rem;
+  border-bottom: 2px solid var(--primary-dark);
+}
+</style>
---
+++ 05-booklistrow-component/src/components/BookListRow.vue
@@ -0,0 +1,22 @@
+<template>
+  <tr class="table-item__table-row">
+    <td>{{ title }}</td>
+    <td>{{ isbn }}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    title: String,
+    isbn: String,
+  },
+};
+</script>
+
+<style scoped>
+.table-item__table td {
+  padding: 12px 15px;
+}
+</style>
